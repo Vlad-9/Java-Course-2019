713298f28ae4cc76acdca7f6766126bc9eb8d2ef
Vlad-9 <swebus999@gmail.com>
2019-10-30 11:02:37 +0300

    pow feature

--- src/app/calc.java
+++ src/app/calc.java
@@ -2,43 +2,7 @@ package app;
 import java.util.Scanner;
 
 public class calc {
-   /* public static void calculate()
-    {
-        System.out.println("Enter first and second number:");
-        Scanner inp= new Scanner(System.in);
-        int num1,num2;
-        num1 = inp.nextInt();
-        num2 = inp.nextInt();
-       //input inp = new input();
-       //inp = inp.setInput();
-       
-       
-        char choose ;
-        operator op1 = new operator();
-        choose = op1.getOperator();
- 
-        switch (choose){
-        case '+':
-            System.out.println(add( num1,num2));
-            break;
-        case '-':
-            System.out.println(sub( num1,num2));
-            break;      
-        case '*':
-            System.out.println(mult( num1,num2));
-            break;
-        case '/':
-            System.out.println(div( num1,num2));
-            break;
-        case '%':
-            System.out.println(mod( num1,num2));
-            break;
-            default:
-                System.out.println("Wrong Operation");
 
-        }
-     
-    }*/
 
     public static double calculate(input inp)
     {
@@ -61,6 +25,9 @@ public class calc {
         case '/':
         ans = div( inp);
             break;
+        case '^':
+        ans = pow( inp);
+                break;
         case '%':
         ans = mod( inp);
             break;
@@ -100,7 +67,11 @@ public class calc {
         double result = inp.getNum1()%inp.getNum2();
         return result;
     }
+    public static int pow(input inp)
+    {
+        int result = (int) Math.pow(inp.getNum1(), inp.getNum2());
+        return result;
+    }
    
     }
 
-gi
